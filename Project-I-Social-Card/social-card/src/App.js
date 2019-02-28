c82dda21cb204efe8b6e42441caad0df2a25c9c1
unknown <kcmp711@gmail.com>
2019-02-27 21:22:46 -0500

    first attempt at HeaderComponent

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./App.css";
 import HeaderContainer from "./components/HeaderComponents/HeaderContainer";
-import CardContainer from "./components/CardComponents/CardContainer";
-import Footer from "./components/FooterComponents/Footer";
+// import CardContainer from "./components/CardComponents/CardContainer";
+// import Footer from "./components/FooterComponents/Footer";
 
 const App = () => {
   return (
     <div>
       <div className="app-container">
         <HeaderContainer />
-        <CardContainer />
-        <Footer />
+        {/* <CardContainer /> */}
+        {/* <Footer /> */}
       </div>
     </div>
   );
